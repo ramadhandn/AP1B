151304d32dd3f729c351d482fa0a9b23f8723091
ramadhandn <91900705+ramadhandn@users.noreply.github.com>
2021-11-28 10:32:35 +0700

    Segitiga decrement added

--- Pertemuan3/src/pertemuan3/Pertemuan3d.java
+++ Pertemuan3/src/pertemuan3/Pertemuan3d.java
@@ -7,12 +7,23 @@ public class Pertemuan3d {
        Scanner W = new Scanner(System.in);
        System.out.print("Masukkan tinggi segitiga : ");
        int t = W.nextInt();
+        
+        // Segitiga Increment
        for(int i = 0; i < t; i++){
            for(int j = 0; j < i; j++){
                System.out.print("@");
            }
            System.out.println("");
        }
+       
+        /* Segitiga Decrement
+       for(int i = t; i > 0; i--){
+           for(int j = 0; j < i; j++){
+               System.out.print("*");
+           }
+           System.out.println("");
+       }
+       */
        
    } 
 }
